7ce8c65037a385fa618ce0b141af65596d3c2292
Yihuai Zhang <2839904Z@student.gla.ac.uk>
2025-12-02 20:31:27 +0800

    feat(mqtt): add incomplete mqtt code

---
+++ components/mqtt/include/my_mqtt.h
@@ -0,0 +1,7 @@
+#ifndef MY_MQTT_H
+#define MY_MQTT_H
+
+
+void mqtt_start(const char* MQTT_TOPIC2,const char* MQTT_ADDRESS,const char* MQTT_CLIENTID,const char* MQTT_USERNAME,const char* MQTT_PASSWORD);
+
+#endif
--- main/SmartIrrigation.c
+++ main/SmartIrrigation.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <my_wifi.h>
+#include <my_mqtt.h>
 
+// WIFI INIT
 #define SSID "NIRVANA"
 #define PASSWORD "NIRVANAFOREVER"
+
+//MQTT INIT
+#define MQTT_ADDRESS "mqtt://broker-cn.emqx.io"
+#define MQTT_CLIENTID "mqtt_esp3220251202"
+#define MQTT_USERNAME "esp32username"
+#define MQTT_PASSWORD "esp32password"
+#define TAG "mqtt"
+
+#define MQTT_TOPIC_ESP32 "/topic/esp32_1123"
+#define MQTT_TOPIC_SUBSCRIBE "/topic/mqttx_1123"
 
 void app_main(void)
 {
